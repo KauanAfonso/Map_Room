d4a9aaa715d11b2e53fdb3c8efaeb9a0ca9b0cae
kauanAfonso <kauanafon3@gmail.com>
2025-05-05 08:51:34 -0300

    Fazendo models e campo de visualização

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import Usuario, Disciplinar, Reserva_ambiente
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+
+
+class UsuarioModelAdmin(UserAdmin):
+    # -------------Informações que aparecerá no painel admin
+    fieldsets = UserAdmin.fields + (
+        (None,{'fields': ('username', 'ni', 'first_name', 'tipo' ),}),
+    )
+    # -------------Informações que aparecerá na criação do objeto no admin
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None,{'fields': ('username','ni', 'first_name', 'tipo' ),}),
+    )
+
+
+class DisciplinaAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fields + (
+        (None,{'fields': ('nome', 'curso', 'descricao', 'professor'),}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None,{'fields': ('nome', 'curso', 'descricao', 'professor'),}),
+    )
+
+
+class ReservaAmbienteAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fields + (
+        (None,{'fields': ('sala_reservada', 'data_inicio', 'data_termino', 'periodo', 'professor'),}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None,{'fields': ('nome', 'curso', 'descricao', 'professor'),}),
+    )
+
+
+class SalaAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fields + (
+        (None,{'fields': ('nome', 'capacidade_alunos'),}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None,{'fields': ('nome', 'capacidade_alunos'),}),
+    )
+
+
+
+
+admin.site.register(Usuario, UsuarioModelAdmin , Disciplinar, DisciplinaAdmin, Reserva_ambiente, ReservaAmbienteAdmin)
--- app/models.py
+++ app/models.py
@@ -3,21 +3,35 @@ from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
     ni = models.BigIntegerField(unique=True)
-    funcionario = models.CharField(max_length=1, choices=(("G","Gestor"), ("P", "Professor")))
-    nome = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=1, choices=(("G","Gestor"), ("P", "Professor")))
     data_nascimento = models.DateField()
     data_contratacao = models.DateField()
+    telefone = models.CharField(max_length=22)
 
 class Disciplinar(models.Model):
     nome = models.CharField()
     curso = models.CharField()
     descricao = models.TextField()
+    professor = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True, blank=True, null=True, limit_choices_to={'tipo': 'P'})
+
+    def __str__(self):
+        return f'{self.nome} {self.get_tipo_display()}'
     
-    professor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+class Sala(model.Model):
+    nome = models.CharField(max_length=100)
+    capacidade_alunos = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+    
 
 class Reserva_ambiente(models.Model):
     data_inicio = models.DateField()
     data_termino = models.DateField()
     periodo = models.CharField(max_length=1, choices=(("M", "Manhã"), ("T", "Tarde"), ("N", "Noite")))
-    sala_reservada = models.CharField(max_length=255)
+    sala_reservada = models.ForeignKey(Sala, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Usuario, on_delete=models.CASCADE, limit_choices_to={'tipo': 'P'})
+
+    def __str__(self):
+        return self.sala_reservada
