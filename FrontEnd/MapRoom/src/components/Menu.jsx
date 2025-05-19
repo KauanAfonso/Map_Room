f37231fe08f6782d707f386eee27959360da8099
kauanAfonso <kauanafon3@gmail.com>
2025-05-19 16:46:36 -0300

    melhorando home

--- FrontEnd/MapRoom/src/components/Menu.jsx
+++ FrontEnd/MapRoom/src/components/Menu.jsx
@@ -4,12 +4,14 @@ import { CardMenu } from "./CardMenu";
 
 export function Menu() {
     return (
+        <>
+        <h1 className={styles.nome}>Olá Kauan AFonso</h1>
         <div className={styles.container}>
             <CardMenu icon={FaBookOpen} label="Disciplinas" />
             <CardMenu icon={FaSchool} label="Ambientes" />
             <CardMenu icon={FaChalkboardTeacher} label="Professores" />
             <CardMenu icon={FaUserTie} label="Gestores" />
         </div>
-        
+        </>
     );
 }
